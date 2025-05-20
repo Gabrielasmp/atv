353bdcc1abca4be6a7204c2639cf2c3bae134079
gabriela sampaio <gabriela.sampaioslva@gmail.com>
2025-05-20 11:24:35 -0300

    Atualização

---
+++ Javascript/exercicios/dolar.js
@@ -0,0 +1,13 @@
+function converter() {
+    const cotacao = 5.00; 
+    const valorDolar = parseFloat(document.getElementById("dolar").value);
+
+    if (!isNaN(valorDolar)) {
+        const valorReal = valorDolar * cotacao;
+        document.getElementById("resultado").innerText = 
+            `Valor em reais: R$ ${valorReal.toFixed(2)}`;
+    } else {
+        document.getElementById("resultado").innerText = 
+            "Por favor, insira um valor válido.";
+    }
+}
